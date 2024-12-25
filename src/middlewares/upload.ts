b3f389b7c04cb03b9e0e7fecdbbea313fbe46f3b
LÊ MINH KHÔI <khoi.kioto@gmail.com>
2024-12-25 14:40:27 +0700

    refactoring uploading

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -17,14 +17,14 @@ const storage = multer.diskStorage({
         cb(null, "uploads/"); // Files will be stored in the "uploads" folder
     },
     filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + getUserId(req);
-        cb(null, `${file.fieldname}-${uniqueSuffix}${path.extname(file.originalname)}`);
+        const uniqueSuffix = getUserId(req);
+        cb(null, `${file.fieldname}-${uniqueSuffix}-${path.extname(file.originalname)}`);
     }
 });
 
 // File filter: Allow only image uploads
 const fileFilter = (req: any, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
-    if (file.mimetype.startsWith('image/')) {
+    if (file.mimetype.startsWith("image/")) {
         cb(null, true);
     } else {
         //@ts-ignore

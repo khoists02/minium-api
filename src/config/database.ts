41e3e3640478bfff0d7582601c365b0b80250643
LÊ MINH KHÔI <khoi.kioto@gmail.com>
2024-12-25 14:15:11 +0700

    add title

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,16 +1,26 @@
+/*
+ * Mimium Pty. Ltd. ("LKG") CONFIDENTIAL
+ * Copyright (c) 2022 Mimium project Pty. Ltd. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of LKG. The intellectual and technical concepts contained
+ * herein are proprietary to LKG and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained
+ * from LKG.  Access to the source code contained herein is hereby forbidden to anyone except current LKG employees, managers or contractors who have executed
+ * Confidentiality and Non-disclosure agreements explicitly covering such access.
+ */
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME || '',
-    process.env.DB_USER || '',
-    process.env.DB_PASSWORD || '',
-    {
-      host: process.env.DB_HOST || "localhost",
-      dialect: "postgres",
-    },
-  );
-  
-  export default sequelize;
+  process.env.DB_NAME || '',
+  process.env.DB_USER || '',
+  process.env.DB_PASSWORD || '',
+  {
+    host: process.env.DB_HOST || "localhost",
+    dialect: "postgres",
+  },
+);
+
+export default sequelize;

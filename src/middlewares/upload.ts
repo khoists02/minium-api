ee620ed90d909fd43aa752342f3b18794bd1293c
LÊ MINH KHÔI <khoi.kioto@gmail.com>
2024-12-25 14:39:29 +0700

    fix

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -8,7 +8,6 @@
  * from LKG.  Access to the source code contained herein is hereby forbidden to anyone except current LKG employees, managers or contractors who have executed
  * Confidentiality and Non-disclosure agreements explicitly covering such access.
  */
-
 import { getUserId } from "@src/utils/authentication";
 import multer from "multer";
 import path from "path";
@@ -18,7 +17,6 @@ const storage = multer.diskStorage({
         cb(null, "uploads/"); // Files will be stored in the "uploads" folder
     },
     filename: (req, file, cb) => {
-        console.log(req.file, { file });
         const uniqueSuffix = Date.now() + '-' + getUserId(req);
         cb(null, `${file.fieldname}-${uniqueSuffix}${path.extname(file.originalname)}`);
     }
